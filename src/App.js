e8356b6994bb08b2dab695d637e53cd16752670e
Arya <32606430+Finura99@users.noreply.github.com>
2023-03-09 16:45:45 +0000

    new way

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import DataTable from "react-data-table-component";
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [perPage, setPerPage] = useState(10)
+
+  const columns = [
+    {
+      name: "Created At",
+      selector: (row) => row.createdAt
+    },
+    {
+      name: "First Name",
+      selector: (row) => row.firstName
+    },{
+      name: "Last Name",
+      selector: (row) => row.LastName
+    },
+    {
+      name: "Id",
+      selector: (row) => row.id
+    }
+  ]
+
+
+useEffect(() => {
+  fetchTableData()
+}, [])
+
+async function fetchTableData() {
+  setLoading(true)
+  const URL = "https://63bd59afce8cd0789c9527f4.mockapi.io/Employment"
+  const response = await fetch(URL)
+
+  const users = await response.json()
+  setData(users)
+  setLoading(false)
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <DataTable 
+        title="Employment (testing)"
+        columns={columns}
+        data={data}
+        progressPending={loading}
+        pagination
+      />
+      
     </div>
   );
 }
