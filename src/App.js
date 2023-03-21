7086946a58f886e109873cc70f6fe0a0f88caf7e
Arya <32606430+Finura99@users.noreply.github.com>
2023-03-21 16:09:29 +0000

    nodatacomponents git

--- src/App.js
+++ src/App.js
@@ -1,78 +1,11 @@
-import DataTable from "react-data-table-component";
-import {useState, useEffect} from "react";
-import Post from "./components/Post";
-import axios from "axios";
+import './App.css';
 
-
-
-export default function App() {
-
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [perPage, setPerPage] = useState(10)
-
-
-  const columns = [
-    {
-      name: "Created At",
-      selector: (row) => row.createdAt
-    },
-    {
-      name: "First Name",
-      selector: (row) => row.firstName
-    },{
-      name: "Last Name",
-      selector: (row) => row.LastName
-    },
-    {
-      name: "Id",
-      selector: (row) => row.id
+    function App() {
+        return (
+            <div className="main">
+                <h3>React CRUD Operations</h3>
+            </div>
+        );
     }
-  ]
 
-  
-
-useEffect(() => {
-  getMethod()
-}, [])
-//get with fetch
-// async function fetchTableData() {
-//   setLoading(true)
-//   const URL = "https://63bd59afce8cd0789c9527f4.mockapi.io/Employment"
-//   const res = await fetch(URL)
-
-//   const users = await res.json()
-//   setData(users)
-// }
-
-async function getMethod() {
-    axios.get("https://63bd59afce8cd0789c9527f4.mockapi.io/Employment")
-    .then(res => {
-    setData(res.data)
-    setLoading(false)
-  })
-    .catch(err => {
-    console.log(err)
-  })
-}
-
-
-return (
-    <div className="App"> 
-      <DataTable 
-        title="Employment Table V1"
-        columns={columns}
-        data={data}
-        progressPending={loading}
-        pagination
-      />
-      <h3>Add an Employee </h3>
-
-      <Post />
-      
-      
-    </div>
-  );
-}
-
-
+export default App
